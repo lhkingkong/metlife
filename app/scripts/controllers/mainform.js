1284bd2259f2b2a8beb34797b5f2ca562440a745
eoparra <edgarparramadrigal@gmal.com>
2018-05-30 17:15:24 -0500

    Add functionality to the form

--- app/scripts/controllers/mainform.js
+++ app/scripts/controllers/mainform.js
@@ -8,7 +8,7 @@
  * Controller of the metLifeApp
  */
 angular.module('metLifeApp')
-    .controller('MainFormCtrl', ['$timeout', '$scope', '$location', 'applicationInfo', 'webServices', function ($timeout, $scope, $location, applicationInfo, webServices) {
+    .controller('MainFormCtrl', ['$timeout', '$scope', '$location', '$filter', 'applicationInfo', 'webServices', function ($timeout, $scope, $location, $filter, applicationInfo, webServices) {
         $scope.section = 1;
         console.log(applicationInfo, 'applicationInfo');
         $scope.editing = false;
@@ -26,6 +26,10 @@ angular.module('metLifeApp')
             startingDay: 1
         };
         $scope.format = 'dd/MM/yyyy';
+        $scope.paymentCards = [{}];
+
+        $scope.basicBenefitOptions = ['1,000,000', '900,000', '800,000', '500,000'];
+        $scope.banks = ['Banamex', 'Bancomer', 'Banorte', 'Scotiabank'];
 
         $scope.additionalBenefits = [
             {
@@ -39,7 +43,7 @@ angular.module('metLifeApp')
                 label: 'Triple Accidental Death',
                 type: 'options',
                 code: 'TAD',
-                options: ['$ 900,000.00', '$ 800,000.00', '$ 700,000.00']
+                options: ['900,000', '800,000', '500,000']
           }
       ];
 
@@ -57,8 +61,46 @@ angular.module('metLifeApp')
         }
 
         $scope.saveNeighborhood = function () {
-            if($scope.selectedNeighborhood !== '-- Otra --'){
+            if ($scope.selectedNeighborhood !== '-- Otra --') {
                 $scope.application.neighborhood = $scope.selectedNeighborhood;
+            }
+        };
+
+        $scope.calculatePremium = function (value) {
+            value += '';
+            value = value.replace(/,/g, "");
+            value = parseFloat(value, 10) * 0.002;
+            return $filter('currency')(value, '$', 2);
+        };
+
+        $scope.calculateTotalPremium = function () {
+            var total = 0, value;
+            if ($scope.application.basicBenefit) {
+                value = $scope.application.basicBenefit + '';
+                value = value.replace(/,/g, "");
+                value = parseFloat(value, 10) * 0.002;
+                total += value;
+            }
+            if ($scope.application.additionalBenefits) {
+                for (var i = 0, len = $scope.additionalBenefits.length; i < len; i++) {
+                    if ($scope.application.additionalBenefits[$scope.additionalBenefits[i].id]) {
+                        value = $scope.application.additionalBenefits[$scope.additionalBenefits[i].id] + '';
+                        value = value.replace(/,/g, "");
+                        value = parseFloat(value, 10) * 0.002;
+                        total += value;
+                    }
+
+                }
+            }
+            return $filter('currency')(total, '$', 2);
+        };
+        
+        $scope.addPaymentCard = function(){
+            $scope.paymentCards.push({});
+        };
+        $scope.removePaymentCard = function(index){
+            if($scope.paymentCards.length>1){
+                $scope.paymentCards.splice(index, 1);
             }
         };
 
